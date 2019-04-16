b59b92365f6cb723c5d84777623edd9eba76adbf
Weirui <kongweirui.zju@gmail.com>
2019-04-16 15:25:44 -0700

    Change database settings.

--- dementia_classifier/db.py
+++ dementia_classifier/db.py
@@ -6,9 +6,9 @@ _connection = None
 def get_connection():
     global _connection
     if not _connection:
-        USER   = 'dementia'
-        PASSWD = 'Dementia123!'
-        DB     = 'masters_data'
+        USER   = 'patata'
+        PASSWD = 'carota'
+        DB     = 'dementia_data'
         url = 'mysql://%s:%s@localhost/%s' % (USER, PASSWD, DB)
         engine = create_engine(url)
         _connection = engine.connect()
